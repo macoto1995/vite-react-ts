ff33ca7c2d8b1242bbb78c21ca85b496036fe52c
Makoto <adosute07@gmail.com>
2022-11-12 14:13:18 +0900

    PokemonIndexPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { 
+  createBrowserRouter, 
+  RouterProvider, 
+} from 'react-router-dom'
+import { PokemonIndexPage } from './pages/pokemon/PokemonPage'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/pokemon",
+    element: <PokemonIndexPage />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
---
+++ src/pages/pokemon/PokemonPage.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from "react"
+import useSWR, {Fetcher} from "swr"
+
+type PokemonListResponse = {
+  count: number
+  next: string
+  results: {
+    name: string
+    url: string
+  }[]
+}
+
+const fetcher: Fetcher<PokemonListResponse, string> = (...args) => fetch(...args).then(res => res.json())
+
+const usePokemonList = () => {
+  const { data, error } = useSWR<PokemonListResponse>('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0', fetcher)
+  const pokemons = useMemo(() => data?.results.map(pokemon => {
+      const id = pokemon.url.split('/').at(-2)
+      const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+      const url = `/pokemon/${id}`
+      return({...pokemon, id, url, imgSrc })
+    }), [data]
+  )
+  return {
+    pokemons,
+    isLoading: !error && !data,
+    isError: !!error
+  }
+}
+
+export const PokemonIndexPage = () => {
+  const { pokemons, isLoading, isError } = usePokemonList()
+  if(isLoading) return <>'...loading'</>
+  if(isError) return <>'Oh no'</>
+  
+  return (
+    <>
+      { pokemons && 
+        pokemons.map(pokemon => (
+          <div key={pokemon.id}>
+            <h4>{pokemon.name}</h4> 
+            <a href={pokemon.url}>{pokemon.name}</a>
+            <img alt={pokemon.name} src={pokemon.imgSrc} />
+          </div>
+        ))
+      }  
+    </>
+  )}
